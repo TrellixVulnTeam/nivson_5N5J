b4800e346c6ee8ef0deddd90af232ea87768703e
Bismuth <90388021+SodiumBismuth@users.noreply.github.com>
2022-03-21 18:50:14 +0100

    erreur requêtes + erreur sur td

--- README.md
+++ README.md
@@ -2,8 +2,13 @@
 
 SI VOUS VOULEZ INSTALLER UNE LIB AVEC PIP DEMANDEZ MOI AVANT DE RAJOUTTER, MERCI !
 
-pour lancer le site, vous devez aller dans une invite de commandes, **IMPOSSIBLE D'ÉXÉCUTER AUTREMENT** : 
+pour lancer le site, vous devez aller dans une invite de commandes, **IMPOSSIBLE D'ÉXÉCUTER AUTREMENT** :
 `../projet/venv/Scripts>activate.bat`
 
 `../projet/code> py manage.py runserver`
 
+## Messages pour les professeurs
+
+Voici différents messages d'erreurs que vous pouvez voir et comment les résoudre :
+
+1 - 
--- projet/code/niveausonore/views.py
+++ projet/code/niveausonore/views.py
@@ -7,96 +7,99 @@ from .td import *
 # partie requêtes
 
 def index(request):
-    return render(request, "index.html")
+    a = ''
+    if request.method == 'GET' and 'test' in request.GET:
+        a = request.GET['test']
+    return render(request, "index.html",context={"exemple":a})
 
 def cour_prem_arbre(request):
-    ctx = request.GET('contexte')
-    day = request.GET('jour')
+    ctx = request.GET['contexte']
+    day = request.GET['jour']
 
 def cour_prem_casier(request):
-    ctx = request.GET('contexte')
-    day = request.GET('jour')
+    ctx = request.GET['contexte']
+    day = request.GET['jour']
 
 def espace_fumeur(request):
-    ctx = request.GET('contexte')
-    day = request.GET('jour')
+    ctx = request.GET['contexte']
+    day = request.GET['jour']
 
 def toilettes_prem(request):
-    ctx = request.GET('contexte')
-    day = request.GET('jour')
+    ctx = request.GET['contexte']
+    day = request.GET['jour']
 
 def salle_cours(request):
-    ctx = request.GET('contexte')
-    day = request.GET('jour')
+    ctx = request.GET['contexte']
+    day = request.GET['jour']
 
 def chapelle(request):
-    ctx = request.GET('contexte')
-    day = request.GET('jour')
+    ctx = request.GET['contexte']
+    day = request.GET['jour']
 
 def salle_devoir_t(request):
-    ctx = request.GET('contexte')
-    day = request.GET('jour')
+    ctx = request.GET['contexte']
+    day = request.GET['jour']
 
 def salle_devoir_p(request):
-    ctx = request.GET('contexte')
-    day = request.GET('jour')
+    ctx = request.GET['contexte']
+    day = request.GET['jour']
 
 def couloir_prem(request):
-    ctx = request.GET('contexte')
-    day = request.GET('jour')
+    ctx = request.GET['contexte']
+    day = request.GET['jour']
 
 def couloir_sd(request):
-    ctx = request.GET('contexte')
-    day = request.GET('jour')
+    ctx = request.GET['contexte']
+    day = request.GET['jour']
 
 def couloir_ter(request):
-    ctx = request.GET('contexte')
-    day = request.GET('jour')
+    ctx = request.GET['contexte']
+    day = request.GET['jour']
 
 def foyer(request):
-    ctx = request.GET('contexte')
-    day = request.GET('jour')
+    ctx = request.GET['contexte']
+    day = request.GET['jour']
 
 def cdi(request):
-    ctx = request.GET('contexte')
-    day = request.GET('jour')
+    ctx = request.GET['contexte']
+    day = request.GET['jour']
 
 def rest_cdi(request):
-    ctx = request.GET('contexte')
-    day = request.GET('jour')
+    ctx = request.GET['contexte']
+    day = request.GET['jour']
 
 def rest_aquarium(request):
-    ctx = request.GET('contexte')
-    day = request.GET('jour')
+    ctx = request.GET['contexte']
+    day = request.GET['jour']
 
 def refectoire(request):
-    ctx = request.GET('contexte')
-    day = request.GET('jour')
+    ctx = request.GET['contexte']
+    day = request.GET['jour']
 
 def sport(request):
-    ctx = request.GET('contexte')
-    day = request.GET('jour')
+    ctx = request.GET['contexte']
+    day = request.GET['jour']
 
 def cour_sd(request):
-    ctx = request.GET('contexte')
-    day = request.GET('jour')
+    ctx = request.GET['contexte']
+    day = request.GET['jour']
 
 def preau_ter(request):
-    ctx = request.GET('contexte')
-    day = request.GET('jour')
+    ctx = request.GET['contexte']
+    day = request.GET['jour']
 
 def ref_prof(request):
-    ctx = request.GET('contexte')
-    day = request.GET('jour')
+    ctx = request.GET['contexte']
+    day = request.GET['jour']
 
 def sdp_pc(request):
-    ctx = request.GET('contexte')
-    day = request.GET('jour')
+    ctx = request.GET['contexte']
+    day = request.GET['jour']
 
 def sdp_distrib(request):
-    ctx = request.GET('contexte')
-    day = request.GET('jour')
+    ctx = request.GET['contexte']
+    day = request.GET['jour']
 
 def sdp_ref(request):
-    ctx = request.GET('contexte')
-    day = request.GET('jour')
+    ctx = request.GET['contexte']
+    day = request.GET['jour']
