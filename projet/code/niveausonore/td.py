51f7fe7fceaa377c85d42f8975691c956f39ed1c
SodiumBismuth <90388021+SodiumBismuth@users.noreply.github.com>
2022-05-02 14:03:39 +0200

    update td

--- projet/code/niveausonore/td.py
+++ projet/code/niveausonore/td.py
@@ -32,22 +32,32 @@ def format_data(D):
                 out.append(j)
     return out
 
-D = format_data(import_data())
-
-def search_data(D,place,ctx,day):
-    a=0
-    b=0
-    c=0
-    d=0
-    for x in D:
-         if x["Lieu"]==place:
-             if x["Conditions "]==ctx:
-                 if x["Jour"]==day:
-                     a=a+x["Valeur minimale"]
-                     b=b+x["Valeur moyenne"]
-                     c=c+x["Valeur maximale"]
-                     d+=1
-    a=a/d
-    b=b/d
-    c=c/d
-    return a,b,c
+def search_data(place,ctx,day): #fct
+    D = format_data(import_data())
+    moymin,moymoy,moymax,eff,maxtemp=0,0,0,0,0 #Initialisation des variables
+    mintemp=999 #Initialisation d'une variable pour avoir le minimum temporaire
+    med,fati,dang=[],[],[] #Initialisation de listes
+    for x in D: #recherche dans le data
+         if x["Lieu"]==place: #correspondance avec le lieu
+             if x["Conditions "]==ctx: #correspondance avec la condition
+                 if x["Jour"]==day: #correspondance avec le jour
+                     moymin,moymoy,moymax=moymin+x["Valeur minimale"],moymoy+x["Valeur moyenne"],moymax+x["Valeur maximale"] #accumulation des valeurs dans les moyennes
+                     eff+=1 #compteur d'effectif
+                     if mintemp>x["Valeur minimale"]:
+                         mintemp=x["Valeur minimale"] #calcul de la valeur minimale
+                     med.append(x["Valeur minimale"]),med.append(x["Valeur moyenne"]),med.append(x["Valeur maximale"]),med.sort() #Création d'un tableau de toutes les valeurs puis trié
+                     if maxtemp<x["Valeur maximale"]:
+                         maxtemp=x["Valeur maximale"] #calcul de la valeur maximale
+                     fati.append([med[i] for i in range(len(med)) if med[i]>=80]) #Création d'un tableau de valeurs au dessus de 80
+                     dang.append([med[i] for i in range(len(med)) if med[i]>=90]) #Création d'un tableau de valeurs au dessus de 90
+    out = {
+        "moymin":round(moymin/eff,1),
+        "moymoy":round(moymoy/eff,1),
+        "moymax":round(moymax/eff,1),
+        "mintemp":mintemp,
+        "med":med[int((len(med)/2)-1)],
+        "maxtemp":maxtemp,
+        "p_fatiguant":round((len(fati[-1])*100)/(len(med)),1),
+        "p_dangereux":round((len(dang[-1])*100)/(len(med)),1)
+          }
+    return out #Arrondi à 10^-1 de la moyenne des valeurs minimales,Arrondi à 10^-1 de la moyenne des valeurs moyennes,Arrondi à 10^-1 de la moyenne des valeurs maximales,Renvoi de la valeur minimale,Renvoi de la valeur médiane, Renvoi de la valeur maximale,Arrondi à 10^-1 de la proportion de valeurs au dessus de 80,Arrondi à 10^-1 de la proportion de valeurs au dessus de 90
