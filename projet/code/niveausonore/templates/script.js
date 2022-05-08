93357aff547fcc7e9d1db82bc97b910f10fe19b7
DissonanceLegale <90622009+DissonanceLegale@users.noreply.github.com>
2022-05-08 16:48:40 +0200

    Update script.js

--- projet/code/niveausonore/templates/script.js
+++ projet/code/niveausonore/templates/script.js
@@ -1,17 +1,11 @@
 function a(el) {
 	var b=document.getElementById(el);
 	if (b.hasAttribute("hidden")) {
-		document.getElementById("espace_fumeur").removeAttribute("hidden");
-		b.style.transition="height 0.2s width 0.2s linear 0s";
-		b.style.width="30%";
-		b.style.height="500px";
+		document.getElementById(el).removeAttribute("hidden");
 	}
 	else {
-		document.getElementById("espace_fumeur").setAttribute("hidden", true);
-		b.style="width=1px","height:1px";
+		document.getElementById(el).setAttribute("hidden", true);
 	}
-	
-	
 }
 
 function b() {
