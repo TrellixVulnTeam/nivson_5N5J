4099134ec96c42bcacc4a68b6eb44ff2cb5399c5
Bismuth <90388021+SodiumBismuth@users.noreply.github.com>
2022-05-11 22:02:05 +0200

    VOUS ABUSEZ

--- projet/code/niveausonore/views.py
+++ projet/code/niveausonore/views.py
@@ -5,9 +5,6 @@
 from django.shortcuts import render
 from .td import *
 
-# partie traitement des données
-
-
 # partie requêtes
 
 def index(request):
@@ -25,8 +22,7 @@ def cour_prem_casier(request):
     if request.method == 'GET' and all(q in request.GET for q in ['condition','jour']):
         cdt = request.GET['condition']
         day = request.GET['jour']
-        mini,moy,maxi = search_data("La cour des premières près des casiers",cdt,day)
-        ctx = {'mini':mini,'moy':moy,'maxi':maxi}
+        ctx = search_data("La cour des premières près des casiers",cdt,day)
         return render(request,'cour_prem_casier.html',context=ctx)
     return render(request,'cour_prem_casier.html')
 
@@ -34,8 +30,7 @@ def espace_fumeur(request):
     if request.method == 'GET' and all(q in request.GET for q in ['condition','jour']):
         cdt = request.GET['condition']
         day = request.GET['jour']
-        mini,moy,maxi = search_data("L'espace \"fumeurs\"",cdt,day)
-        ctx = {'mini':mini,'moy':moy,'maxi':maxi}
+        ctx = search_data("L'espace \"fumeurs\"",cdt,day)
         return render(request,'espace_fumeur.html',context=ctx)
     return render(request,'espace_fumeur.html')
 
@@ -43,8 +38,7 @@ def toilettes_prem(request):
     if request.method == 'GET' and all(q in request.GET for q in ['condition','jour']):
         cdt = request.GET['condition']
         day = request.GET['jour']
-        mini,moy,maxi = search_data("Les toilettes des premières",cdt,day)
-        ctx = {'mini':mini,'moy':moy,'maxi':maxi}
+        ctx = search_data("Les toilettes des premières",cdt,day)
         return render(request,'toilettes_prem.html',context=ctx)
     return render(request,'toilettes_prem.html')
 
@@ -52,8 +46,7 @@ def salle_cours(request):
     if request.method == 'GET' and all(q in request.GET for q in ['condition','jour']):
         cdt = request.GET['condition']
         day = request.GET['jour']
-        mini,moy,maxi = search_data("Une salle de cours",cdt,day)
-        ctx = {'mini':mini,'moy':moy,'maxi':maxi}
+        ctx = search_data("Une salle de cours",cdt,day)
         return render(request,'salle_cours.html',context=ctx)
     return render(request,'salle_cours.html')
 
@@ -61,8 +54,7 @@ def chapelle(request):
     if request.method == 'GET' and all(q in request.GET for q in ['condition','jour']):
         cdt = request.GET['condition']
         day = request.GET['jour']
-        mini,moy,maxi = search_data("La salle de devoirs \"La chapelle\"",cdt,day)
-        ctx = {'mini':mini,'moy':moy,'maxi':maxi}
+        ctx = search_data("La salle de devoirs \"La chapelle\"",cdt,day)
         return render(request,'chapelle.html',context=ctx)
     return render(request,'chapelle.html')
 
@@ -70,8 +62,7 @@ def salle_devoir_t(request):
     if request.method == 'GET' and all(q in request.GET for q in ['condition','jour']):
         cdt = request.GET['condition']
         day = request.GET['jour']
-        mini,moy,maxi = search_data("La salle de devoirs \"T301\"",cdt,day)
-        ctx = {'mini':mini,'moy':moy,'maxi':maxi}
+        ctx = search_data("La salle de devoirs \"T301\"",cdt,day)
         return render(request,'salle_devoir_t.html',context=ctx)
     return render(request,'salle_devoir_t.html')
 
@@ -79,8 +70,7 @@ def salle_devoir_p(request):
     if request.method == 'GET' and all(q in request.GET for q in ['condition','jour']):
         cdt = request.GET['condition']
         day = request.GET['jour']
-        mini,moy,maxi = search_data("La salle de devoirs \"P302\"",cdt,day)
-        ctx = {'mini':mini,'moy':moy,'maxi':maxi}
+        ctx = search_data("La salle de devoirs \"P302\"",cdt,day)
         return render(request,'salle_devoir_p.html',context=ctx)
     return render(request,'salle_devoir_p.html')
 
@@ -88,8 +78,7 @@ def couloir_prem(request):
     if request.method == 'GET' and all(q in request.GET for q in ['condition','jour']):
         cdt = request.GET['condition']
         day = request.GET['jour']
-        mini,moy,maxi = search_data("Un couloir du batiment 1ères",cdt,day)
-        ctx = {'mini':mini,'moy':moy,'maxi':maxi}
+        ctx = search_data("Un couloir du batiment 1ères",cdt,day)
         return render(request,'couloir_prem.html',context=ctx)
     return render(request,'couloir_prem.html')
 
@@ -97,8 +86,7 @@ def couloir_sd(request):
     if request.method == 'GET' and all(q in request.GET for q in ['condition','jour']):
         cdt = request.GET['condition']
         day = request.GET['jour']
-        mini,moy,maxi = search_data("Un couloir du batiment 2ndes",cdt,day)
-        ctx = {'mini':mini,'moy':moy,'maxi':maxi}
+        ctx = search_data("Un couloir du batiment 2ndes",cdt,day)
         return render(request,'couloir_sd.html',context=ctx)
     return render(request,'couloir_sd.html')
 
@@ -106,8 +94,7 @@ def couloir_ter(request):
     if request.method == 'GET' and all(q in request.GET for q in ['condition','jour']):
         cdt = request.GET['condition']
         day = request.GET['jour']
-        mini,moy,maxi = search_data("Un couloir du batiment terminales",cdt,day)
-        ctx = {'mini':mini,'moy':moy,'maxi':maxi}
+        ctx = search_data("Un couloir du batiment terminales",cdt,day)
         return render(request,'couloir_ter.html',context=ctx)
     return render(request,'couloir_ter.html')
 
@@ -115,8 +102,7 @@ def foyer(request):
     if request.method == 'GET' and all(q in request.GET for q in ['condition','jour']):
         cdt = request.GET['condition']
         day = request.GET['jour']
-        mini,moy,maxi = search_data("Le foyer",cdt,day)
-        ctx = {'mini':mini,'moy':moy,'maxi':maxi}
+        ctx = search_data("Le foyer",cdt,day)
         return render(request,'foyer.html',context=ctx)
     return render(request,'foyer.html')
 
@@ -124,8 +110,7 @@ def cdi(request):
     if request.method == 'GET' and all(q in request.GET for q in ['condition','jour']):
         cdt = request.GET['condition']
         day = request.GET['jour']
-        mini,moy,maxi = search_data("Le CDI",cdt,day)
-        ctx = {'mini':mini,'moy':moy,'maxi':maxi}
+        ctx = search_data("Le CDI",cdt,day)
         return render(request,'cdi.html',context=ctx)
     return render(request,'cdi.html')
 
@@ -133,8 +118,7 @@ def rest_cdi(request):
     if request.method == 'GET' and all(q in request.GET for q in ['condition','jour']):
         cdt = request.GET['condition']
         day = request.GET['jour']
-        mini,moy,maxi = search_data("Dans la file d'attente pour la restauration près du CDI",cdt,day)
-        ctx = {'mini':mini,'moy':moy,'maxi':maxi}
+        ctx = search_data("Dans la file d'attente pour la restauration près du CDI",cdt,day)
         return render(request,'rest_cdi.html',context=ctx)
     return render(request,'rest_cdi.html')
 
@@ -142,8 +126,7 @@ def rest_aquarium(request):
     if request.method == 'GET' and all(q in request.GET for q in ['condition','jour']):
         cdt = request.GET['condition']
         day = request.GET['jour']
-        mini,moy,maxi = search_data("Dans la file d'attente pour la restauration \"Aquarium\"",cdt,day)
-        ctx = {'mini':mini,'moy':moy,'maxi':maxi}
+        ctx = search_data("Dans la file d'attente pour la restauration \"Aquarium\"",cdt,day)
         return render(request,'rest_aquarium.html',context=ctx)
     return render(request,'rest_aquarium.html')
 
@@ -151,8 +134,7 @@ def refectoire(request):
     if request.method == 'GET' and all(q in request.GET for q in ['condition','jour']):
         cdt = request.GET['condition']
         day = request.GET['jour']
-        mini,moy,maxi = search_data("Le réfectoire des élèves",cdt,day)
-        ctx = {'mini':mini,'moy':moy,'maxi':maxi}
+        ctx = search_data("Le réfectoire des élèves",cdt,day)
         return render(request,'refectoire.html',context=ctx)
     return render(request,'refectoire.html')
 
@@ -160,8 +142,7 @@ def sport(request):
     if request.method == 'GET' and all(q in request.GET for q in ['condition','jour']):
         cdt = request.GET['condition']
         day = request.GET['jour']
-        mini,moy,maxi = search_data("Le complexe sportif",cdt,day)
-        ctx = {'mini':mini,'moy':moy,'maxi':maxi}
+        ctx = search_data("Le complexe sportif",cdt,day)
         return render(request,'sport.html',context=ctx)
     return render(request,'sport.html')
 
@@ -169,8 +150,7 @@ def cour_sd(request):
     if request.method == 'GET' and all(q in request.GET for q in ['condition','jour']):
         cdt = request.GET['condition']
         day = request.GET['jour']
-        mini,moy,maxi = search_data("La cour des secondes ",cdt,day)
-        ctx = {'mini':mini,'moy':moy,'maxi':maxi}
+        ctx = search_data("La cour des secondes ",cdt,day)
         return render(request,'cour_sd.html',context=ctx)
     return render(request,'cour_sd.html')
 
@@ -178,8 +158,7 @@ def preau_ter(request):
     if request.method == 'GET' and all(q in request.GET for q in ['condition','jour']):
         cdt = request.GET['condition']
         day = request.GET['jour']
-        mini,moy,maxi = search_data("Sous le préau de la cour des terminales",cdt,day)
-        ctx = {'mini':mini,'moy':moy,'maxi':maxi}
+        ctx = search_data("Sous le préau de la cour des terminales",cdt,day)
         return render(request,'preau_ter.html',context=ctx)
     return render(request,'preau_ter.html')
 
@@ -187,8 +166,7 @@ def ref_prof(request):
     if request.method == 'GET' and all(q in request.GET for q in ['condition','jour']):
         cdt = request.GET['condition']
         day = request.GET['jour']
-        mini,moy,maxi = search_data("Le réfectoire des professeurs batiment P",cdt,day)
-        ctx = {'mini':mini,'moy':moy,'maxi':maxi}
+        ctx = search_data("Le réfectoire des professeurs batiment P",cdt,day)
         return render(request,'ref_prof.html',context=ctx)
     return render(request,'ref_prof.html')
 
@@ -196,8 +174,7 @@ def sdp_pc(request):
     if request.method == 'GET' and all(q in request.GET for q in ['condition','jour']):
         cdt = request.GET['condition']
         day = request.GET['jour']
-        mini,moy,maxi = search_data("La salle des profs (coins ordinateurs ) batiment L",cdt,day)
-        ctx = {'mini':mini,'moy':moy,'maxi':maxi}
+        ctx = search_data("La salle des profs (coins ordinateurs ) batiment L",cdt,day)
         return render(request,'sdp_pc.html',context=ctx)
     return render(request,'sdp_pc.html')
 
@@ -205,8 +182,7 @@ def sdp_distrib(request):
     if request.method == 'GET' and all(q in request.GET for q in ['condition','jour']):
         cdt = request.GET['condition']
         day = request.GET['jour']
-        mini,moy,maxi = search_data("La salle des profs (coin distributeur) batiment L",cdt,day)
-        ctx = {'mini':mini,'moy':moy,'maxi':maxi}
+        ctx = search_data("La salle des profs (coin distributeur) batiment L",cdt,day)
         return render(request,'sdp_distrib.html',context=ctx)
     return render(request,'sdp_distrib.html')
 
@@ -214,7 +190,6 @@ def sdp_ref(request):
     if request.method == 'GET' and all(q in request.GET for q in ['condition','jour']):
         cdt = request.GET['condition']
         day = request.GET['jour']
-        mini,moy,maxi = search_data("La salle des profs (réfectoire) batiment L",cdt,day)
-        ctx = {'mini':mini,'moy':moy,'maxi':maxi}
+        ctx = search_data("La salle des profs (réfectoire) batiment L",cdt,day)
         return render(request,'sdp_ref.html',context=ctx)
     return render(request,'sdp_ref.html')
